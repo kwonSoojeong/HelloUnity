6dcee6708d04669c1f6aeef1065cef82dd5b8038
kwonSoojeong <0401tnwjd@naver.com>
2017-11-25 15:20:00 +0900

    둥굴게둥굴게 오류 수정

--- Assets/02.Script/ComFollow.cs
+++ Assets/02.Script/ComFollow.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComFollow : MonoBehaviour {
+public class ComFollow : MonoBehaviour
+{
     public Transform Player;
     private Vector3 Offset;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Offset = transform.position - Player.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 Playerpos = Player.position + Offset;
         transform.position = Vector3.Slerp(transform.position, Playerpos, Time.deltaTime * 5f);
-	}
+    }
 }
+
--- Assets/02.Script/PlayerCtrl.cs
+++ Assets/02.Script/PlayerCtrl.cs
@@ -1,34 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//클래스는 System.Serializable이라는 어트리뷰트(Attribute)를 명시해야 Inspector 뷰에 노출됨
+[System.Serializable]
+public class Anim
+{
+    public AnimationClip idle;
+    public AnimationClip runForward;
+    public AnimationClip runBackward;
+    public AnimationClip runRight;
+    public AnimationClip runLeft;
+}
 
-public class PlayerCtrl : MonoBehaviour {
+public class PlayerCtrl : MonoBehaviour
+{
     private float h = 0.0f;
     private float v = 0.0f;
-
-    public Transform tr;
-    public float moveSpeed = 10.0f; //유니티화면에서 변수변경 가능
-    //마우스 카메라 회전 속도 
+    //자주 사용하는 컴포넌트는 반드시 변수에 할당한 후 사용
+    private Transform tr;
+    //이동 속도 변수
+    public float moveSpeed = 100.0f;
+    //회전 속도 변수
     public float rotSpeed = 1000.0f;
-
-    // Use this for initialization
-    void Start () {
+    //인스펙터뷰에 표시할 애니메이션 클래스 변수
+    public Anim anim;
+    //아래에 있는 3D 모델의 Animation 컴포넌트에 접근하기 위한 변수
+    public Animation _animation;
+    void Start()
+    {
+        //스크립트 처음에 Transform 컴포넌트 할당
         tr = GetComponent<Transform>();
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        //자신의 하위에 있는 Animation 컴포넌트를 찾아와 변수에 할당
+        _animation = GetComponentInChildren<Animation>();
+        //Animation 컴포넌트의 애니메이션 클립을 지정하고 실행
+        _animation.clip = anim.idle;
+        _animation.Play();
+    }
+    void Update()
+    {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
-        Debug.Log("H = " + h.ToString());
-        Debug.Log("V = " + v.ToString());
-        //전후좌우 이동 방향 벡터계산
+        //Debug.Log("H = " + h.ToString());
+        //Debug.Log("V = " + v.ToString());
+        //전후좌우 이동 방향 벡터 계산
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
-        //Translate(이동방향 * 델타타임*변위값*속도,기준좌표)
-        tr.Translate(moveDir * Time.deltaTime * moveSpeed, Space.Self); //셀프는 동서남북 신경안쓰고 움직임
-
-        //마우스 화면 회전
+        //Translate(이동 방향 * Time.deltaTime * 변위값 * 속도, 기준좌표)
+        tr.Translate(moveDir * Time.deltaTime * moveSpeed, Space.Self);
+        //Vector3.up 축을 기준으로 rotSpeed만큼의 속도로 회전
         tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+        //키보드 입력값을 기준으로 동작할 애니메이션 수행
+        if (v >= 0.1f)
+        {
+            //전진 애니메이션
+            _animation.CrossFade(anim.runForward.name, 0.3f);
+        }
+        else if (v <= -0.1f)
+        {
+            //후진 애니메이션
+            _animation.CrossFade(anim.runBackward.name, 0.3f);
+        }
+        else if (h >= 0.1f)
+        {
+            //오른쪽 이동 애니메이션
+            _animation.CrossFade(anim.runRight.name, 0.3f);
+        }
+        else if (h <= -0.1f)
+        {
+            //왼쪽 이동 애니메이션
+            _animation.CrossFade(anim.runLeft.name, 0.3f);
+        }
+        else
+        {
+            //정지시 idle애니메이션
+            _animation.CrossFade(anim.idle.name, 0.3f);
+        }
+
     }
 }
+
