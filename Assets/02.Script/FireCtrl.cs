01fb569000b9efb058f826753f77614cbed9c3da
kwonSoojeong <0401tnwjd@naver.com>
2017-11-25 17:01:15 +0900

    BulletCtrl, Fire, 등 누락파일들 추가

---
+++ Assets/02.Script/FireCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FireCtrl : MonoBehaviour
+{
+    //총알 프리팹
+    public GameObject bullet;
+    //총알 발사좌표
+    public Transform firePos;
+    void Update()
+    {
+        //마우스 왼쪽 버튼을 클릭했을 때 Fire 함수 호출
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        //동적으로 총알을 생성하는 함수
+        CreateBullet();
+    }
+    void CreateBullet()
+    {
+        //Bullet 프리팹을 동적으로 생성
+        Instantiate(bullet, firePos.position, firePos.rotation);
+    }
+}
